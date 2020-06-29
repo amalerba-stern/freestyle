f350a67515a10ff217527f5ac8e2c29fa6315686
amalerba-stern <65737752+amalerba-stern@users.noreply.github.com>
2020-06-29 17:48:54 -0400

    Expand input table

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,4 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-my_spend = [] # initialize list
-
 APP_ENV = os.getenv("APP_ENV", default="development") # use "production" on a remote server
--- web_app/routes/budget_routes.py
+++ web_app/routes/budget_routes.py
@@ -2,7 +2,6 @@
 
 from flask import Blueprint, render_template, request
 
-
 budget_routes = Blueprint("budget_routes", __name__)
 
 @budget_routes.route("/budget/form")
@@ -11,15 +10,24 @@ def budget_form():
     return render_template("budget_form.html")
 
 @budget_routes.route("/budget/input-received", methods=["POST"])
-def budget_input_received():
+def budget_input_received(my_spend=[]):
     print("RECEIVED YOUR BUDGET FORM INPUTS...")
-
     if request.method == 'POST':
         print("FORM DATA:", dict(request.form))
         item = request.form['item']
         category = request.form['category']
         price = request.form['price']
         month = request.form['month']
+
+        input_dict = dict({"item":item, "category":category, 
+                   "price": price, "month":month})
+        try:
+            my_spend.append(input_dict)
+        except:
+            my_spend = []
+            my_spend.append(input_dict)
+
+    print("MY SPEND:", my_spend)
     return render_template("budget_input_received.html", item=item, 
                             category=category, price=price, month=month)
 
